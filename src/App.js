0b54d645d933afb08e1eebf2a83a23b82ba95d2f
Jose Vte. Boronat <jvboronat@gmail.com>
2022-01-18 15:25:52 +0100

    GitHub Finder Project Start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import About from './pages/About'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -7,10 +11,17 @@ function App() {
       
       <div className="flex flex-col justify-between h-screen">
       <Navbar/>
-      <main>
+            <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home/>}></Route>
+              <Route path='/about' element={<About/>}></Route>
+              <Route path='/notfound' element={<NotFound/>}></Route>
+              <Route path='/*' element={<NotFound/>}></Route>
+            
+            </Routes>
+          </main>
+      <Footer/>      
 
-        Content
-      </main>
       </div>
       
       
